3b062431f07e90d7dec2772498ebcb58187cf4b4
Jared Miller <jared@smell.flowers>
2024-02-22 16:56:43 -0500

    Ditch the smurf icon for a zoidberg

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ let iconChanged = false;
 
 function resetIconOnTabLoad(_tabId, changeInfo, _tab) {
   if (changeInfo.status === "complete" && iconChanged) {
-    browser.browserAction.setIcon({ path: "icons/smurf.png" });
+    browser.browserAction.setIcon({ path: "icons/zoidberg.png" });
     iconChanged = false;
     browser.tabs.onUpdated.removeListener(resetIconOnTabLoad);
   }
@@ -10,7 +10,7 @@ function resetIconOnTabLoad(_tabId, changeInfo, _tab) {
 
 browser.browserAction.onClicked.addListener(async () => {
   const tabs = await browser.tabs.query({ active: false });
-  browser.browserAction.setIcon({ path: "icons/nosmurf.png" });
+  browser.browserAction.setIcon({ path: "icons/noidberg.png" });
   iconChanged = true;
   for (const tab of tabs) {
     await browser.tabs.discard(tab.id);
