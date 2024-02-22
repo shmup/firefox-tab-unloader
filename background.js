d91f359093cdc145e8395108d2051a0a491755cc
Jared Miller <jared@smell.flowers>
2024-02-22 12:56:18 -0500

    Restore smurf icon when another tab loads

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
+let iconChanged = false;
+
+function resetIconOnTabLoad(_tabId, changeInfo, _tab) {
+  if (changeInfo.status === "complete" && iconChanged) {
+    browser.browserAction.setIcon({ path: "icons/smurf.png" });
+    iconChanged = false;
+    browser.tabs.onUpdated.removeListener(resetIconOnTabLoad);
+  }
+}
+
 browser.browserAction.onClicked.addListener(async () => {
   const tabs = await browser.tabs.query({ active: false, currentWindow: true });
   if (tabs.length > 0) {
+    browser.browserAction.setIcon({ path: "icons/nosmurf.png" });
+    iconChanged = true;
     for (const tab of tabs) {
       await browser.tabs.discard(tab.id);
     }
-    browser.browserAction.setIcon({ path: "icons/nosmurf.png" });
+    browser.tabs.onUpdated.addListener(resetIconOnTabLoad);
   }
 });
