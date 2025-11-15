646326aa2ae86bc609e4d78670eb6c215efc6052
Jared Miller <jared@smell.flowers>
2025-11-15 12:54:26 -0500

    Update manifest and code from v2 to v3

--- background.js
+++ background.js
@@ -2,18 +2,18 @@ let iconChanged = false;
 
 function resetIconOnTabLoad(_tabId, changeInfo, _tab) {
   if (changeInfo.status === "complete" && iconChanged) {
-    browser.browserAction.setIcon({ path: "icons/zoidberg.png" });
+    browser.action.setIcon({ path: "icons/zoidberg.png" });
     iconChanged = false;
-    browser.tabs.onUpdated.removeListener(resetIconOnTabLoad); // remove listener
+    browser.tabs.onUpdated.removeListener(resetIconOnTabLoad);
   }
 }
 
-browser.browserAction.onClicked.addListener(async () => {
+browser.action.onClicked.addListener(async () => {
   const tabs = await browser.tabs.query({ active: false });
-  browser.browserAction.setIcon({ path: "icons/noidberg.png" });
+  browser.action.setIcon({ path: "icons/voidberg.png" });
   iconChanged = true;
   for (const tab of tabs) {
     await browser.tabs.discard(tab.id);
   }
-  browser.tabs.onUpdated.addListener(resetIconOnTabLoad); // add listener
+  browser.tabs.onUpdated.addListener(resetIconOnTabLoad);
 });
